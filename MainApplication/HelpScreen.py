17b5fcfe4f81145817a309d5d59cc9cd86cb8ed6
Zak Groenewold <zekery360@gmail.com>
2024-11-23 16:51:15 -0600

    Add try/except for help document.

--- MainApplication/HelpScreen.py
+++ MainApplication/HelpScreen.py
@@ -6,16 +6,19 @@ def open_help_window():
     new_window.geometry("700x400")
     new_window.title("Help")
 
-    def open_help():
-        file_path = "./HelpDoc.txt"
-        with open(file_path, "r") as file:
-            text_area.delete("1.0", tk.END)
-            text_area.insert(tk.END, file.read())
+    def open_help_doc():
+        file_path = "./MainApplication/HelpDoc.txt"
+        try:
+            with open(file_path, "r") as file:
+                text_area.delete("1.0", tk.END)
+                text_area.insert(tk.END, file.read())
+        except FileNotFoundError:
+            print("ERROR: Help Document not found.")
 
     text_area = scrolledtext.ScrolledText(new_window, height=22)
     text_area.pack()
 
-    to_main_button = tk.Button(new_window, text="Back", command=new_window.destroy)
+    to_main_button = tk.Button(new_window, text="Close", command=new_window.destroy)
     to_main_button.pack(side='bottom')
 
-    open_help()
+    open_help_doc()
