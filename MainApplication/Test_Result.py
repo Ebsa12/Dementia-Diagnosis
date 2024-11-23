0b9ff8a14fad597e7a04b106f6b1296b08f5c7b7
Jarso, Ebsa K <md5220xm@go.minnstate.edu>
2024-11-23 17:17:51 +0000

    Updated Test_Result.py

--- MainApplication/Test_Result.py
+++ MainApplication/Test_Result.py
@@ -5,17 +5,17 @@ import subprocess
 import APIConnection
 
 
-# Function to simulate running the model again
-def run_model_again():
-    messagebox.showinfo("Close Application", "The Application will now close.")
-    subprocess.Popen(['python3', 'LoadingScreen.py'])
+# Function to close the application
+def close_application():
+    messagebox.showinfo("Close Application", "The application will now close.")
     root.destroy()
 
-# Create the the result application windo
+
+# Create the result application window
 root = Tk()
 root.title("Results Page")
 
-        # Set the window size
+# Set the window size
 root.geometry("700x400")
 root.resizable(False, False)
 
@@ -29,30 +29,30 @@ elif 25 <= PosDiagnosis < 50:
 else:
     risk_message = f"You have little to no risk ({PosDiagnosis}%) of contracting Dementia."
 
-    # Create and pack the main title label
+# Create and pack the main title label
 titleText = StringVar()
-titleText.set(f"You have a {PosDiagnosis}% change of contracting Dementia")
+titleText.set(risk_message)
 title_label = Label(root, textvariable=titleText, font=("Arial", 16), fg="lightgreen")
 title_label.pack(pady=20)
 
-    # Create and pack the disclaimer label
+# Create and pack the disclaimer label
 disclaimer_label = Label(
-        root,
-        text=(
+    root,
+    text=(
         "Disclaimer: This is not a diagnosis of Dementia.\n"
-            "While the application has indicated you may be at\n"
-            "risk of Dementia, do not seek any form of treatment\n"
-            "until consulting a doctor or medical professional."
-        ),
-        font=("Arial", 10),
-        fg="#666",
-        justify="center",
-    )
+        "While the application has indicated you may be at\n"
+        "risk of Dementia, do not seek any form of treatment\n"
+        "until consulting a doctor or medical professional."
+    ),
+    font=("Arial", 10),
+    fg="#666",
+    justify="center",
+)
 disclaimer_label.pack(pady=10)
 
-        # Create and pack the "Close application" button
+# Create and pack the "Close Application" button
 close_button = Button(root, text="Close Application", font=("Arial", 12), fg="#4CAF50", command=close_application)
 close_button.pack(pady=20)
 
-        # Start the Tkinter event loop
+# Start the Tkinter event loop
 root.mainloop()
